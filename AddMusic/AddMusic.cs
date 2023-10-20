1c7d6004c428f08a5d22838bf184feff7d4db39d
miworky <miworky@gmail.com>
2023-10-20 15:23:25 +0900

    パラメータを設定するUIを追加

--- AddMusic/AddMusic.cs
+++ AddMusic/AddMusic.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Windows.Forms;
 using ScriptPortal.Vegas;
 
+using System.Drawing;
+
 namespace AddMusic
 {
 
@@ -18,6 +20,17 @@ namespace AddMusic
                 MessageBox.Show("Error: No tracks");
                 return;
             }
+
+            // パラメータを設定するダイアログを開く
+            Tuple<DialogResult, string, string> dialogResult = DoDialog(vegas);
+            DialogResult result = dialogResult.Item1;
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            string VideoTrackNo1BaseString = dialogResult.Item2;
+            string MusicTrackNo1BaseString = dialogResult.Item3;
 
             // ダイアログを開き出力するログのファイルパスをユーザーに選択させる
             string saveFilePath = GetFilePath(vegas.Project.FilePath, "AddMusic");
@@ -40,25 +53,42 @@ namespace AddMusic
             //            audioFiles.Show(writer);
 
 
-            // トラックに張り付けてあるメディアをみる
-            // 画像が 2 つ以上連続していれば、その区間に音楽をつける
-            // 想定しているトラック構成：
-            // 0: テロップ
-            // 1: ビデオ
-            // 2: ビデオの音声（ゲイン補正あり）
-            // 3: ビデオの音声（ゲイン補正なし）
-            // 4: 音楽
-            const int VideoTrackNo = 1;
-            const int MusicTrackNo = 4;
-
-            if (vegas.Project.Tracks.Count <= MusicTrackNo)
+ 
+            int VideoTrackNo1Base = 1;
+            if (!int.TryParse(VideoTrackNo1BaseString, out VideoTrackNo1Base))
             {
-                MessageBox.Show("Error: Requires 5 tracks or more.");
+                MessageBox.Show("Cannot parse VideoTrackNoString");
+                return;
+            }
+            if (VideoTrackNo1Base == 0)
+            {
+                MessageBox.Show("VideoTrackNoString must not be 0.");
+                return;
+            }
+            int VideoTrackNo0Base = VideoTrackNo1Base - 1;
+
+            int MusicTrackNo1Base = 4;
+            if (!int.TryParse(MusicTrackNo1BaseString, out MusicTrackNo1Base))
+            {
+                MessageBox.Show("Cannot parse MusicTrackNoString");
+                return;
+            }
+            if (MusicTrackNo1Base == 0)
+            {
+                MessageBox.Show("MusicTrackNoString must not be 0.");
                 return;
             }
 
-            Track videoTrack = vegas.Project.Tracks[VideoTrackNo];
-            Track musicTrack = vegas.Project.Tracks[MusicTrackNo];
+            int MusicTrackNo0Base = MusicTrackNo1Base - 1;
+
+            if ((vegas.Project.Tracks.Count <= VideoTrackNo0Base) || (vegas.Project.Tracks.Count <= MusicTrackNo0Base))
+            {
+                MessageBox.Show("Error: Requires more tracks.");
+                return;
+            }
+
+            Track videoTrack = vegas.Project.Tracks[VideoTrackNo0Base];
+            Track musicTrack = vegas.Project.Tracks[MusicTrackNo0Base];
 
             //{
 
@@ -167,6 +197,126 @@ namespace AddMusic
             string[] Extentions = new string[] { ".jpg", ".jpeg", ".heic" };
 
             return isSameExtention(path, Extentions);
+        }
+
+
+        private Tuple<DialogResult, string, string> DoDialog(Vegas vegas)
+        {
+            Form form = new Form();
+            form.SuspendLayout();
+            form.AutoScaleMode = AutoScaleMode.Font;
+            form.AutoScaleDimensions = new SizeF(6F, 13F);
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterParent;
+            form.MaximizeBox = false;
+            form.MinimizeBox = false;
+            form.HelpButton = false;
+            form.ShowInTaskbar = false;
+            form.Text = "Add Music";
+            form.AutoSize = true;
+            form.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+
+            TableLayoutPanel layout = new TableLayoutPanel();
+            layout.AutoSize = true;
+            layout.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            layout.GrowStyle = TableLayoutPanelGrowStyle.AddRows;
+            layout.ColumnCount = 3;
+            layout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 140));
+            layout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 180));
+            layout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 80));
+            form.Controls.Add(layout);
+
+            {
+                Label label;
+
+                label = new Label();
+                label.Text = "Video Track No(1-base):";
+                label.AutoSize = false;
+                label.TextAlign = ContentAlignment.MiddleLeft;
+                label.Margin = new Padding(8, 8, 8, 4);
+                label.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+                layout.Controls.Add(label);
+                layout.SetColumnSpan(label, 3);
+            }
+
+            TextBox VideoTrackNoBox = new TextBox();
+            {
+                VideoTrackNoBox.Text = "2";
+
+                VideoTrackNoBox.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+                VideoTrackNoBox.Margin = new Padding(16, 8, 8, 4);
+                layout.Controls.Add(VideoTrackNoBox);
+                layout.SetColumnSpan(VideoTrackNoBox, 2);
+            }
+
+            {
+                Label label;
+
+                label = new Label();
+                label.Text = "Music Track No(1-base):";
+                label.AutoSize = false;
+                label.TextAlign = ContentAlignment.MiddleLeft;
+                label.Margin = new Padding(8, 8, 8, 4);
+                label.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+                layout.Controls.Add(label);
+                layout.SetColumnSpan(label, 3);
+            }
+
+            TextBox MusicTrackNoBox = new TextBox();
+            {
+                MusicTrackNoBox.Text = "5";
+
+                MusicTrackNoBox.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+                MusicTrackNoBox.Margin = new Padding(16, 8, 8, 4);
+                layout.Controls.Add(MusicTrackNoBox);
+                layout.SetColumnSpan(MusicTrackNoBox, 2);
+            }
+
+
+
+            {
+                FlowLayoutPanel buttonPanel = new FlowLayoutPanel();
+                buttonPanel.FlowDirection = FlowDirection.RightToLeft;
+                buttonPanel.Size = Size.Empty;
+                buttonPanel.AutoSize = true;
+                buttonPanel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+                buttonPanel.Margin = new Padding(8, 8, 8, 8);
+                buttonPanel.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+                layout.Controls.Add(buttonPanel);
+                layout.SetColumnSpan(buttonPanel, 3);
+
+                {
+                    Button cancelButton = new Button();
+                    cancelButton.Text = "Cancel";
+                    cancelButton.FlatStyle = FlatStyle.System;
+                    cancelButton.DialogResult = DialogResult.Cancel;
+                    buttonPanel.Controls.Add(cancelButton);
+                    form.CancelButton = cancelButton;
+                }
+
+                {
+                    Button okButton = new Button();
+
+                    okButton.Text = "OK";
+                    okButton.FlatStyle = FlatStyle.System;
+                    okButton.DialogResult = DialogResult.OK;
+                    buttonPanel.Controls.Add(okButton);
+                    form.AcceptButton = okButton;
+                }
+            }
+
+            form.ResumeLayout();
+
+            string textVideoTrackNo = "";
+            string textMusicTrackNo = "";
+            DialogResult result = form.ShowDialog(vegas.MainWindow);
+            if (DialogResult.OK == result)
+            {
+                textVideoTrackNo = VideoTrackNoBox.Text;
+                textMusicTrackNo = MusicTrackNoBox.Text;
+            }
+
+            return Tuple.Create(result, textVideoTrackNo, textMusicTrackNo);
         }
 
     }
